0258ad6afade58814fb5eb24b6fd8384504eec8d
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2026-05-12 12:31:40 -0700

    Bussiness exception created

---
+++ CleanTheet.Domain/Exceptions/BussinessRuleException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanTheet.Domain.Exceptions;
+
+public class BussinessRuleException : Exception
+{
+    public BussinessRuleException(string message) : base(message)
+    {
+    }
+}
