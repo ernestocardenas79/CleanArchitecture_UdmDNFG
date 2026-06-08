86f47b4a28c77c4425277f199169c1e5f209315c
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2026-06-08 10:50:04 -0700

    Create Dental offices controller and test it out.

---
+++ CleanTeeth.API/Controllers/DentalOfficesController.cs
@@ -0,0 +1,42 @@
+using CleanTeeth.API.DTOs.DentalOffices;
+using CleanTeeth.Application.Features.DentalOffices.Commands.CreateDentalOffice;
+using CleanTeeth.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+using CleanTeeth.Application.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanTeeth.API.Controllers;
+
+[ApiController]
+[Route("api/dentaloffices")]
+public class DentalOfficesController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public DentalOfficesController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(CreateDentalOfficeDTO createDentalOfficeDTO)
+    {
+        var command = new CreateDentalOfficeCommand
+        {
+            Name = createDentalOfficeDTO.Name
+        };
+
+        await mediator.Send(command);
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<DentalOfficeDetailDTO>> Get(Guid id)
+    {
+        var query = new GetDentalOfficeDetailQuery
+        {
+            Id = id
+        };
+        var result = await mediator.Send(query);
+        return Ok(result);
+    }
+}
---
+++ CleanTeeth.API/DTOs/DentalOffices/CreateDentalOfficeDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanTeeth.API.DTOs.DentalOffices;
+
+public class CreateDentalOfficeDTO
+{
+    public string Name { get; internal set; }
+}
