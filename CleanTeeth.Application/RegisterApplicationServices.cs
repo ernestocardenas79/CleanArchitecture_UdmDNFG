83fe59065e894e4be4dac97d7a83c1e53e172db2
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2026-06-07 21:45:10 -0700

    Create the registration extension to inclue the project easily in the main project

---
+++ CleanTeeth.Application/RegisterApplicationServices.cs
@@ -0,0 +1,18 @@
+using CleanTeeth.Application.Features.DentalOffices.Commands.CreateDentalOffice;
+using CleanTeeth.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+using CleanTeeth.Application.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanTeeth.Application;
+
+public static class RegisterApplicationServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddTransient<IMediator, SimpleMediator>();
+        services.AddScoped<IRequestHandler<CreateDentalOfficeCommand,Guid>, CreateDentalOfficeCommandHandler>();
+        services.AddScoped<IRequestHandler<GetDentalOfficeDetailQuery, DentalOfficeDetailDTO>, GetDentalOfficeDetailQueryHandler>();
+        
+        return services;
+    }
+}
