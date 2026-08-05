385ae0a643053ef92257897e4ae9d5fb837afda4
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2026-08-05 15:21:30 -0700

    Avoid register Handlers manually instead of that will be registered automatically with Scrutor.

--- CleanTeeth.Application/RegisterApplicationServices.cs
+++ CleanTeeth.Application/RegisterApplicationServices.cs
@@ -1,8 +1,3 @@
-using CleanTeeth.Application.Features.DentalOffices.Commands.CreateDentalOffice;
-using CleanTeeth.Application.Features.DentalOffices.Commands.DeleteDentalOffice;
-using CleanTeeth.Application.Features.DentalOffices.Commands.UpdateDentalOffice;
-using CleanTeeth.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
-using CleanTeeth.Application.Features.DentalOffices.Queries.GetDentalOfficesList;
 using CleanTeeth.Application.Utilities;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,12 +8,15 @@ public static class RegisterApplicationServices
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddTransient<IMediator, SimpleMediator>();
-        services.AddScoped<IRequestHandler<CreateDentalOfficeCommand,Guid>, CreateDentalOfficeCommandHandler>();
-        services.AddScoped<IRequestHandler<GetDentalOfficeDetailQuery, DentalOfficeDetailDTO>, GetDentalOfficeDetailQueryHandler>();
-        services.AddScoped<IRequestHandler<GetDentalOfficesListQuery, List<DentalOfficesListDTO>>, GetDentalOfficesListQueryHandler>();
-        services.AddScoped<IRequestHandler<UpdateDentalOfficeCommand>, UpdateDentalOfficeCommandHandler>();
-        services.AddScoped<IRequestHandler<DeleteDentalOfficeCommand>, DeleteDentalOfficeCommandHandler>();
-        
+
+        services.Scan(scan => scan.FromAssembliesOf(typeof(RegisterApplicationServices))
+            .AddClasses(classes => classes.AssignableTo(typeof(IRequestHandler<>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            .AddClasses(c => c.AssignableTo(typeof(IRequestHandler<,>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
         return services;
     }
 }
